1b89d67cdc267fea88cba5f1d69e8fb52610eb4d
tyommik <2fasti.miem@gmail.com>
2019-05-11 19:57:43 +0300

    Simple video archive synapsis.

--- events.py
+++ events.py
@@ -6,7 +6,7 @@ import cv2
 Detection = namedtuple('Detection', 'pos, x, y, w, h, obj_class, precision')
 
 video_input = r'timecompressor_archive.mp4'
-video_output = r'summary.mp4'
+video_output = r'summary_3.mp4'
 cap = cv2.VideoCapture(video_input)
 cap.set(2, 1500)
 
@@ -32,6 +32,10 @@ class Event:
     @property
     def stop(self):
         return self.__stop or self.detections[-1].pos
+
+    @property
+    def length(self):
+        return self.stop - self.start
 
 
 class VideoCapture:
@@ -66,6 +70,13 @@ class VideoCapture:
                 return frame
             else:
                 print('No frame')
+
+    def get_time(self):
+        f = self.get_position()
+        hours = int(f // 90000)
+        minutes = int((f - hours * 90000) // 1500)
+        secondes = int((f - hours * 90000 - minutes * 1500) // 25)
+        return "{0:02d}:{1:02d}:{2:02d}".format(hours, minutes, secondes)
 
 
 def read_file():
@@ -113,83 +124,61 @@ def main(events):
         h = int(cap.get(4))
         fourcc = cv2.VideoWriter_fourcc(*'MJPG')
         out = cv2.VideoWriter(video_output, fourcc, 25, (w, h))
-        frame_index = -1
 
-    video = VideoCapture(video_source=video_input, start_frame=0)
-    start_pos = 0
 
-    for idx, event in enumerate(events):
+    while events:
 
-        video.set_position(start_pos)
+        stack = []
 
-        gap_step = (event.start - start_pos) // (1 * 25)
+        for _ in range(10):
+            stack.append(events.pop(0))
 
-        total = 30
-        (rAvg, gAvg, bAvg, mAvg) = (None, None, None, None)
-        # проматываем на ускорении гап между событиями
-        # накладываем кадры друг на друга, чтобы было плавно
-        while start_pos < event.start - 3 * 25:
-            print(video.get_position())
+        stack = list(reversed(sorted(stack, key=lambda x: x.length)))
 
-            frame = video.get_frame()
-            (B, G, R) = cv2.split(frame.astype("float"))
-            # if the frame averages are None, initialize them
-            if rAvg is None:
-                rAvg = R
-                bAvg = B
-                gAvg = G
+        shift = 3 * 25
+        videos = [VideoCapture(video_source=video_input, start_frame=event.start - shift) for event in stack]
 
-            # otherwise, compute the weighted average between the history of
-            # frames and the current frames
-            else:
-                rAvg = ((total * rAvg) + (1 * R)) / (total + 1.0)
-                gAvg = ((total * gAvg) + (1 * G)) / (total + 1.0)
-                bAvg = ((total * bAvg) + (1 * B)) / (total + 1.0)
+        max_length = stack[0].length + 2 * shift
 
-            # increment the total number of frames read thus far
-            # total += 1
+        while max_length:
+            frames = [video.get_frame() for video in videos]
 
-            avg = cv2.merge([bAvg, gAvg, rAvg]).astype("uint8")
+            background = frames[0]
 
-            start_pos += gap_step
-            video.set_position(start_pos)
-            cv2.imshow('heatmap', avg)
+            for idx, event in enumerate(stack, start=0):
+                detections = event.detections_map.get(videos[idx].get_position())
+                if detections:
+                    for detection in detections:
+                        # cv2.rectangle(background, (int(detection.x), int(detection.y)),
+                        #               (int(detection.x + detection.w), int(detection.y + detection.h)),
+                        #               (255, 255, 255), 2)
+
+                        x = detection.x
+                        y = detection.y
+                        w = detection.w
+                        h = detection.h
+                        background[y:y + h, x:x + w] = frames[idx][y:y + h, x:x + w]
+
+                    for detection in detections:
+                        cv2.putText(background, videos[idx].get_time(), (int(detection.x), int(detection.y)), 0,
+                                    5e-3 * 200, (0, 255, 0), 2)
+
+
+            # res_show = cv2.addWeighted(frames[0], 0.5, frames[1], 0.5, 2)
+            # res_show = cv2.addWeighted(res_show, 0.5, frames[2], 0.5, 2)
+
+            # frame = video.get_frame()
+            cv2.imshow('test', background)
+
+            max_length -= 1
 
             if writeVideo_flag:
                 # save a frame
-                out.write(frame)
+                out.write(background)
 
             k = cv2.waitKey(1) & 0xff
             if k == 27:
                 break
-
-        while video.get_position() <= event.stop + 3 * 25:
-            t1 = time.time()
-
-            frame = video.get_frame()
-
-            detections = event.detections_map.get(video.get_position())
-            if detections:
-                for detection in detections:
-                    print('Video frame: ', video.get_position(), 'event_frame: ', detection.pos)
-                    cv2.rectangle(frame, (int(detection.x), int(detection.y)), (int(detection.x + detection.w), int(detection.y + detection.h)),
-                                  (255, 255, 255), 2)
-                    # cv2.putText(frame, str(detection.obj_class_name), (int(detection.x), int(detection.y)), 0, 5e-3 * 200, (0, 255, 0), 2)
-
-            cv2.imshow('heatmap', frame)
-
-            fps = (fps + (1. / (time.time() - t1))) / 2
-            print("fps= %f" % (fps))
-
-            if writeVideo_flag:
-                # save a frame
-                out.write(frame)
-
-            k = cv2.waitKey(30) & 0xff
-            if k == 27:
-                break
-
-        start_pos = video.get_position() + 1
 
     # writer.close()
     cap.release()
